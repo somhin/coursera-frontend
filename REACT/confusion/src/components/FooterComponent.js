01ef412c1b5437a7486691c2a283602208bf0c77
Phipat Limprasutr <phipat.limprasutr@outlook.com>
2020-08-31 20:01:15 +0700

    added React-router fixed header,footer

--- REACT/confusion/src/components/FooterComponent.js
+++ REACT/confusion/src/components/FooterComponent.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Footer(props) {
     return(
@@ -8,10 +9,10 @@ function Footer(props) {
                 <div className="col-4 offset-1 col-sm-2">
                     <h5>Links</h5>
                     <ul className="list-unstyled">
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">About</a></li>
-                        <li><a href="#">Menu</a></li>
-                        <li><a href="contactus.html">Contact</a></li>
+                        <li><Link to="/home">Home</Link></li>
+                        <li><Link to="/menu">Menu</Link></li>
+                        <li><Link to="/about">About</Link></li>
+                        <li><Link to="/contact">Contact</Link></li>
                     </ul>
                 </div>
                 <div className="col-7 col-sm-5">
@@ -28,11 +29,8 @@ function Footer(props) {
                 </div>
                 <div className="col-12 col-sm-4 align-self-center">
                     <div className="text-center">
-                        <a className="btn btn-social-icon btn-google" href="http://google.com/+"><i className="fa fa-google-plus"></i></a>
                         <a className="btn btn-social-icon btn-facebook" href="http://www.facebook.com/profile.php?id="><i className="fa fa-facebook"></i></a>
                         <a className="btn btn-social-icon btn-linkedin" href="http://www.linkedin.com/in/"><i className="fa fa-linkedin"></i></a>
-                        <a className="btn btn-social-icon btn-twitter" href="http://twitter.com/"><i className="fa fa-twitter"></i></a>
-                        <a className="btn btn-social-icon btn-google" href="http://youtube.com/"><i className="fa fa-youtube"></i></a>
                         <a className="btn btn-social-icon" href="mailto:"><i className="fa fa-envelope-o"></i></a>
                     </div>
                 </div>
--- REACT/confusion/src/components/MainComponent.js
+++ REACT/confusion/src/components/MainComponent.js
@@ -1,32 +1,38 @@
 import React, { Component } from 'react';
-import Header from './HeaderComponent'
-import Footer from './FooterComponent'
+import Home from './HomeComponent';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 import Menu from './MenuComponent';
-import DishDetail from './DishdetailComponent'
+import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
-
+import { Switch, Route, Redirect } from 'react-router-dom'
 
 class Main extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      dishes: DISHES,
-      selectedDish: null
+      dishes: DISHES
     }
   }
 
-  onDishSelect(dishId) {
-    this.setState({ selectedDish:dishId });
-}
-
   render(){
+
+    const HomePage = () => {
+      return(
+        <Home />
+      )
+    }
     return (
       <div>
         <Header />
-        <Menu dishes={this.state.dishes}
-            onClick={(dishId) => this.onDishSelect(dishId)} />
-        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+
+        <Switch>
+          <Route path="/home" component={ HomePage } />
+          <Route exact path="/menu" component={ () => <Menu dishes={this.state.dishes} /> } />
+          <Redirect to="/home" />
+        </Switch>
+
         <Footer />
       </div>
     );
