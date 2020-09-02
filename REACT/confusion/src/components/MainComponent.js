9587749a3cb8004043ffdbe9d264db5d2e7b3988
Phipat Limprasutr <phipat.limprasutr@outlook.com>
2020-09-02 18:50:47 +0700

    assignment 2

--- REACT/confusion/src/components/MainComponent.js
+++ REACT/confusion/src/components/MainComponent.js
@@ -3,6 +3,7 @@ import Home from './HomeComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Menu from './MenuComponent';
+import About from './AboutComponent'
 import Contact from './ContactComponent'
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments'
@@ -48,6 +49,7 @@ class Main extends Component {
           <Route path="/home" component={ HomePage } />
           <Route exact path="/menu" component={ () => <Menu dishes={this.state.dishes} /> } />
           <Route path="/menu/:dishId" component={ DishWithId } />
+          <Route exact path="/about" component={()=><About leaders={this.state.leaders}/>} />
           <Route exact path="/contact" component={ Contact } />
           <Redirect to="/home" />
         </Switch>
