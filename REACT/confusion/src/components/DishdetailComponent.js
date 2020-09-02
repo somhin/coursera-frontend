04738f36053c0c79997f379209b6ef354ac45aff
Phipat Limprasutr <phipat.limprasutr@outlook.com>
2020-09-02 17:18:48 +0700

    good state part2 unfinish

--- REACT/confusion/src/components/DishdetailComponent.js
+++ REACT/confusion/src/components/DishdetailComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, BreadcrumbItem, Breadcrumb } from 'reactstrap';
+import { Link } from 'react-router-dom'
 
 const DishDetail = (props) => {
     const {dish} = props;
--- REACT/confusion/src/components/MenuComponent.js
+++ REACT/confusion/src/components/MenuComponent.js
@@ -1,19 +1,34 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom'
 
 const Menu = (props) => {
     const menu = props.dishes.map((dish) => {
         return(
             <div key={dish.id} className='col-12 col-md-5 m-1'>
-                <RenderMenuItem dish={dish} onClick={props.onClick}/>
+                <RenderMenuItem dish={dish}/>
             </div>
         );
     });
     
     return(
         <div className='container'>
+            <div className="row">
+                <Breadcrumb>
+                    <BreadcrumbItem>
+                        <Link to='home'>Home</Link>
+                    </BreadcrumbItem>
+                    <BreadcrumbItem active>
+                        Menu
+                    </BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>Menu</h3>
+                    <hr />
+                </div>
+            </div>
             <div className='row'>
-                    {menu}
+                {menu}
             </div>
         </div>
     );
@@ -21,11 +36,13 @@ const Menu = (props) => {
 
 function RenderMenuItem({ dish, onClick }) {
     return (
-    <Card onClick={() => onClick(dish.id)}>
-        <CardImg width="100%" object src={dish.image} alt={dish.name}/>
-        <CardImgOverlay>
-            <CardTitle>{dish.name}</CardTitle>
-        </CardImgOverlay>
+    <Card>
+        <Link to={`/menu/${dish.id}`} >
+            <CardImg width="100%" object src={dish.image} alt={dish.name}/>
+            <CardImgOverlay>
+                <CardTitle>{dish.name}</CardTitle>
+            </CardImgOverlay>
+        </Link>
     </Card>
     );
 }
